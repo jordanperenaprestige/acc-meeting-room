7e4afac61d901c98dff636bd2fb9a92642e77a46
jordanperenaprestige <jperena.prestigeinteractive@gmail.com>
2023-10-25 15:21:30 +0800

    updates - kiosk ipad mode

--- resources/views/layout/kiosk_default/master.blade.php
+++ resources/views/layout/kiosk_default/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"> -->
     <meta name="viewport" content="width=device-width, minimal-ui">
     <meta name="apple-mobile-web-app-capable" content="black">
     <title>PRESTIGE INTERACTIVE</title>
